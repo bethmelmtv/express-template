252c2d52ce5b64ac1cc430f75040c00f10cd840a
Beth <bethmelmtv@gmail.com>
2022-08-09 14:35:26 -0700

    + signup and signup with duplicate email to test

---
+++ tests/auth.test.js
@@ -0,0 +1,29 @@
+const { setupDb, signUpUser } = require('./utils.js');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('/api/v1/auth', () => {
+  beforeEach(setupDb);
+
+  it('/signup', async () => {
+    const { agent, user, credentials } = await signUpUser();
+
+    expect(user).toEqual({
+      id: expect.any(String),
+      email: credentials.email,
+    });
+
+    const { statusCode } = await agent.get('/api/v1/auth/verify');
+    expect(statusCode).toBe(200);
+  });
+
+  it('/signup with duplicate email', async () => {
+    await signUpUser();
+    const { res } = await signUpUser();
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toEqual({
+      status: 400,
+      message: 'Email already exists',
+    });
+  });
+});
